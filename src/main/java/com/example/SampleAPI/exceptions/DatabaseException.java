f6a4dfa7bb916dfa180eb62325f6aa284275efaa
melika barzegaran hosseini <melika.barzegaran.hosseini@gmail.com>
2016-07-31 21:42:00 +0430

    Add some exceptions

---
+++ src/main/java/com/example/SampleAPI/exceptions/DatabaseException.java
@@ -0,0 +1,26 @@
+package com.example.SampleAPI.exceptions;
+
+public class DatabaseException extends RuntimeException
+{
+    public DatabaseException(){}
+
+    public DatabaseException(String message)
+    {
+        super(message);
+    }
+
+    public DatabaseException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+    public DatabaseException(Throwable cause)
+    {
+        super(cause);
+    }
+
+    public DatabaseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+    {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
